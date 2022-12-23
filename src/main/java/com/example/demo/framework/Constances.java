f80d19bbaea26f06de48c09806852279d564e4fb
admin <admin@DESKTOP-7D86IBB>
2022-12-23 17:38:55 +0900

    commit

--- src/main/java/com/example/demo/framework/Constances.java
+++ src/main/java/com/example/demo/framework/Constances.java
@@ -2,4 +2,8 @@ package com.example.demo.framework;
 
 public class Constances {
 	private static String DEMO_STR = "";
+	
+	// database setting 
+	private static String BASE_PACKAGE_PREFIX = "com.example.demo.bialogic.*.dao ";
+	private static String BASE_MAPPER_PATH = "classpath:mapper/*.xml";
 }
---
+++ src/main/java/com/example/demo/framework/db/DataBaseConfig.java
@@ -0,0 +1,10 @@
+package com.example.demo.framework.db;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Mapper
+public class DataBaseConfig {
+	
+	
+}	
